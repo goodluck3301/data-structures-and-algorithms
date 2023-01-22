0c66e2109b1e06a42d7c018e753b81fd7a31be90
Levon M <100533325+goodluck3301@users.noreply.github.com>
2023-01-22 17:30:33 +0400

    Update LinkedHashMap.kt

--- DataStructureInternalWorking/LinkedHashMap.kt
+++ DataStructureInternalWorking/LinkedHashMap.kt
@@ -41,4 +41,50 @@ class LinkedHashMap<K, V> {
         } else {
             // Create a new node for the key-value pair
             val newNode = Node(key, value)
-            //
+            // Add the new node to the tail of the linked list
+            addToTail(newNode)
+            // Add the new node to the hash table
+            table[key] = newNode
+        }
+    }
+
+    // Get the value for a key
+    fun get(key: K): V? {
+        // Look up the key in the hash table
+        val node = table[key]
+        if (node != null) {
+            // Move the node to the tail of the linked list
+            moveToTail(node)
+            // Return the value
+            return node.value
+        }
+        // Return null if the key is not in the map
+        return null
+    }
+
+    // Remove the oldest key-value pair from the map
+    fun removeOldest(): K? {
+        // Get the node at the head of the linked list
+        val oldestNode = head
+        if (oldestNode != null) {
+            // Remove the node from the hash table
+            table.remove(oldestNode.key)
+            // Remove the node from the linked list
+            removeFromList(oldestNode)
+            // Return the key
+            return oldestNode.key
+        }
+        // Return null if the map is empty
+        return null
+    }
+    
+    // Move a node to the tail of the linked list
+    private fun moveToTail(node: Node<K, V>) {
+        // Remove the node from its current position in the linked list
+        removeFromList(node)
+        // Add the node to the tail of the linked list
+        addToTail(node)
+    }
+
+    // Add a node to the tail of the linked list
+    private fun addToTail
